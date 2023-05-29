3bd4bfe02feb0414ab846f230873b39ea853690b
Spy <119489004+amthespy@users.noreply.github.com>
2023-05-29 09:49:51 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = config("13750040", default=None, cast=int)
-API_HASH = config("6553ea819bb17098b0e3c62530823328", default=None)
-BOT_TOKEN = config("6052295632:AAG3SR53VHX66YdMz0YMa8-DSzypwRT5leA", default=None)
-SESSION = config("BQBnFfyHF-PhuexNUyPkwt5Y2UkDQg8eCOtBidb39UomEQxdqE75b9K1VuNrhDwvENSvdt-jBfbK9v-fgWyFnbzjae4HcGIBgfZQjn_K6-Sijw4gFvwGUSWGfE3AgVGygm4amJGOfr1hykdUZFZkAuVnHPLBcoais1CPjFPYD8jdy5eeD67nSt8NxNRls23nMelp4h_cs0hwljNugWkHC9_oOVoYdC0Cc-IAMsD1M_03mJLC3iHPH5hncuVswCORrd_OXakUDQS8u5wZBVfwuG27rhUVK4oT6k__N9m5zcgHOctlUDQ05pv-cSBClAq3vOV0O8yDBGmepanGSN0V5bwrAAAAAXUZulgA", default=None)
-FORCESUB = config("amthespy", default=None)
-AUTH = config("5842877813", default=None, cast=int)
+API_ID = "13750040"
+API_HASH = "6553ea819bb17098b0e3c62530823328"
+BOT_TOKEN = "6052295632:AAG3SR53VHX66YdMz0YMa8-DSzypwRT5leA"
+SESSION = "BQBnFfyHF-PhuexNUyPkwt5Y2UkDQg8eCOtBidb39UomEQxdqE75b9K1VuNrhDwvENSvdt-jBfbK9v-fgWyFnbzjae4HcGIBgfZQjn_K6-Sijw4gFvwGUSWGfE3AgVGygm4amJGOfr1hykdUZFZkAuVnHPLBcoais1CPjFPYD8jdy5eeD67nSt8NxNRls23nMelp4h_cs0hwljNugWkHC9_oOVoYdC0Cc-IAMsD1M_03mJLC3iHPH5hncuVswCORrd_OXakUDQS8u5wZBVfwuG27rhUVK4oT6k__N9m5zcgHOctlUDQ05pv-cSBClAq3vOV0O8yDBGmepanGSN0V5bwrAAAAAXUZulgA"
+FORCESUB = "amthespy"
+AUTH = "5842877813"
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
